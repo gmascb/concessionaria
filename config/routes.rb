f360baf8f02c6f3db87dea3275e6c657994d9ae7
Guilherme <gmascb@gmail.com>
2016-07-13 16:52:51 +0000

    Login como Tela Inicial

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,8 @@ Rails.application.routes.draw do
   resources :users
   get 'page/index'
 
-  root 'page#index'
+  #root 'page#index'
+  root 'sessions#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
