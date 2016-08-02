6da2650036a0b05bf7df665bd9d433b8004979ed
Guilherme <gmascb@gmail.com>
2016-08-02 03:07:23 +0000

    i18n & outros

--- app/models/ficha.rb
+++ app/models/ficha.rb
@@ -4,4 +4,14 @@ class Ficha < ActiveRecord::Base
   belongs_to :Customer
   belongs_to :Cars
   belongs_to :MeioPagamento
+  
+  validates :Vendedor, presence: true
+  validates :Banco, presence: true
+  validates :Customer, presence: true
+  validates :Cars, presence: true
+  validates :MeioPagamento, presence: true
+  validates :coef, presence: true
+  validates :valfinanceiro, presence: true
+  validates :valorparcela, presence: true
+  validates :valortotal, presence: true
 end
